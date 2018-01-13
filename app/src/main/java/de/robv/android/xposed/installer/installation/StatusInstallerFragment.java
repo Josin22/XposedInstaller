5ad303cb99eb05c1a57e7d700d7bcc11561c0cab
维术 <weishu.tws@alipay.com>
2018-01-13 12:03:40 +0800

    [Exposed]: Disable install hits (VAExposed has no this warning)

--- app/src/main/java/de/robv/android/xposed/installer/installation/StatusInstallerFragment.java
+++ app/src/main/java/de/robv/android/xposed/installer/installation/StatusInstallerFragment.java
@@ -114,7 +114,7 @@ public class StatusInstallerFragment extends Fragment {
         refreshKnownIssue(v);
 
         // Display warning dialog to new users
-        if (!XposedApp.getPreferences().getBoolean("hide_install_warning", false)) {
+        if (false && !XposedApp.getPreferences().getBoolean("hide_install_warning", false)) {
             new MaterialDialog.Builder(getActivity())
                     .title(R.string.install_warning_title)
                     .content(R.string.install_warning)
